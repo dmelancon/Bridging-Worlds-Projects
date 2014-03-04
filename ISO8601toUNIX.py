b347b0bfd263057582cc36295971f3f5631119d0
mr.dmelancon@gmail.com <dan@172-29-2-70.dynapool.nyu.edu>
2014-03-04 11:19:41 -0500

    added file

--- ISO8601toUNIX.py
+++ ISO8601toUNIX.py
@@ -3,7 +3,7 @@ import time
 #This simple script will take an EVENT time in ISO8601 and return the time in seconds until that event
 ISOtime = "2014-03-04T13:11:00-05:00"  #time of event in ISO8601  
  
-#strip the timezone because time.mktime alreadu assumes local time
+#strip the timezone because time.mktime already assumes local time
 ISOtime = ISOtime.replace(ISOtime[-6:],"")
 #convert ISOtime to UNIX timecode
 UNIXtime = time.mktime(time.strptime(ISOtime,'%Y-%m-%dT%H:%M:%S'))
